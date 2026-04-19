6db343f0d113237003f14fca0dec019897cd5eed
DilumPal <palawaththadilum@gmail.com>
2026-04-19 13:16:40 +0530

    .env implementation

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import userRouter from "./routes/userRoutes.js";
 import jwt from "jsonwebtoken";
 import orderRouter from "./routes/orderRoute.js";
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
@@ -37,7 +39,7 @@ app.use(
         //next();
     })
 
-mongoose.connect("mongodb+srv://admin:123@cluster0.oigmmnt.mongodb.net/?appName=Cluster0")
+mongoose.connect(process.env.MONGODB_URL)
     .then(() => {
         console.log("Connected to database");
     }).catch(() => {
