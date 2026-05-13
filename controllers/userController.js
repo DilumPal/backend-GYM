f7dcab63b600d95897323a7038fe2800c86c1794
DilumPal <palawaththadilum@gmail.com>
2026-05-13 19:39:10 +0530

    google login update

--- controllers/userController.js
+++ controllers/userController.js
@@ -93,6 +93,88 @@ export async function loginWithGoogle(req, res){
         }
     })
     console.log(response.data)
+
+    const user = await User.findOne({
+        email: response.data.email
+    })
+    /*
+    email : {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName : {
+        type : String,
+        required: true
+    },
+    lastName : {
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    role : {
+        type : String,
+        required : true,
+        default : "customer"
+    },
+    isBlocked : {
+        type : Boolean,
+        required : true,
+        default : false
+    },
+    img : {
+        type : String,
+        required : false,
+        default : "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=2048x2048&w=is&k=20&c=6hQNACQQjktni8CxSS_QSPqJv2tycskYmpFGzxv3FNs="
+    }
+    */
+
+    if(user == null){
+        const newUser = new User(
+            {
+                email: response.data.email,
+                firstName: response.data.given_name,
+                lastName: response.data.family_name,
+                password: "googleUser",
+                img: response.data.picture
+            }
+        )
+        await newUser.save();
+        const token = jwt.sign(
+            {
+                email: newUser.email,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                role: newUser.role,
+                img: newUser.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login Successful",
+            token: token,
+            role: newUser.role
+        })
+    }else{
+        const token = jwt.sign(
+            {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+                img: user.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login Successful",
+            token: token,
+            role: user.role
+        })
+    }
 }
 
 export function isAdmin(req){
