f5333725f38d388c6b2637b61208040a6bb579cb
DilumPal <palawaththadilum@gmail.com>
2026-05-13 15:46:04 +0530

    Add google login

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import axios from "axios";
 
 export function createUser(req, res) {
 
@@ -76,6 +77,22 @@ export function loginUser(req, res) {
                 }
             }
         })
+}
+
+export async function loginWithGoogle(req, res){
+    const token = req.body.accessToken;
+    if(token == null){
+        res.status(400).json({
+            message: "Access token is required"
+        });
+        return;
+    }
+    const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+    console.log(response.data)
 }
 
 export function isAdmin(req){
